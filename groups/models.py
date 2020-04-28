59d09336017b2c69b43f3dcaf99ef7052f20089a
Ernest Macharia <ernesthuku@gmail.com>
2020-04-28 11:14:28 +0300

    First Commit

---
+++ groups/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+# Create your models here.
+
+from django import template
+register = template.Library()
+User = get_user_model
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(blank=True, default='')
+    description_html = models.TextField(editable=False, default='', blank=True)
+    #members = models.ForeignKey(User,on_delete = models.CASCADE,through="GroupMember")
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("groups:single", kwargs={"slug": self.slug})
+
+
+    class Meta:
+        ordering = ["name"]
+	
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group, on_delete = models.CASCADE,related_name="memberships")
+    #user = models.ForeignKey(User,on_delete = models.CASCADE,related_name='user_groups')
+
+    #def __str__(self):
+        #return self.user.username
+
+    class Meta:
+        unique_together = ["group"]
